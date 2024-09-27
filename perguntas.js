10707a5e89a24378efa4bd00cdc55045bb2832e2
Anthony H <149083900+gouveiaanthony56@users.noreply.github.com>
2024-09-27 15:44:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao (
+    'Hardware',
+    'Qual a função da placa-mãe?',
+    'A placa-mãe é a espinha dorsal que une os componentes do computador em um ponto e permite que eles conversem entre si.'
+)
+criaCartao (
+    'Hardware',
+   'Como é feito um hardware?',
+    'Ele é formado por circuitos elétricos e comandado por um sistema operacional e por softwares.'
+)
+
+criaCartao (
+    'Hardware',
+   'Qual o processador mais vendido no mundo ?',
+    'Intel Core i5-3470'
+)
+criaCartao (
+    'Hardware',
+   'Por que a Intel está em declínio ?',
+    ' A publicação do balanço da Intel no 2º trimestre de 2024 foi o gatilho para o tombo.'
+)
+
